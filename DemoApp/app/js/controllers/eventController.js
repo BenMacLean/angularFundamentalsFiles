9c0272c57e5828f3fb8fa2e9d039cfa966711e99
BenMacLean <benhmaclean@gmail.com>
2016-04-25 15:17:49 -0300

    image in and padded

--- DemoApp/app/js/controllers/eventController.js
+++ DemoApp/app/js/controllers/eventController.js
@@ -6,7 +6,13 @@ eventsApp.controller('eventController',
     $scope.event = {
       name: 'Angular Boot Camp',
       date: '1/1/2013',
-      time: '10:30 am'
+      time: '10:30 am',
+      location: {
+        address: 'Google HQ',
+        city: 'Mountain View',
+        province: 'CA'
+      },
+      imageURL: '/img/angularjs-logo.png'
     }
   }
 );
